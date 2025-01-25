02b78c711b34f61132111ed0ac0a41eab8b9e2ed
MarcelloPescatore <pescatoremarcello@gmail.com>
2025-01-25 01:09:01 +0100

    add order to results

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -8,6 +8,8 @@ export default function AppMain() {
     const [isCollapsed, setIsCollapsed] = useState(false);
     const [selectedCategory, setSelectedCategory] = useState(null);
     const [labelShow, setLabelShow] = useState(false)
+    const [selectedLabel, setSelectedLabel] = useState('Autore')
+    const [selectedIcon, setSelectedIcon] = useState('bi-list-task')
 
     // Logica per filtrare i dati
     const filteredData = dataList.filter((item) => {
@@ -33,6 +35,12 @@ export default function AppMain() {
     function handleToggleLabel() {
         setLabelShow(!labelShow)
     }
+
+    const handleLabelClick = (label, icon) => {
+        setSelectedLabel(label);
+        setSelectedIcon(icon);
+        setLabelShow(false);
+    };
 
     return (
         <div className="row">
@@ -63,7 +71,7 @@ export default function AppMain() {
                     />
 
                     {/* search */}
-                    <div className="second-part d-flex flex-column justify-content-between gap-3 ">
+                    <div className="search-part d-flex flex-column gap-3 ">
                         <div className="search d-flex justify-content-between align-items-center">
                             {inputSearch ? (
                                 <div className="input-search d-flex align-items-center">
@@ -86,56 +94,99 @@ export default function AppMain() {
 
                             {/* Ordina per */}
                             <div className="filters d-flex align-items-center gap-2 position-relative">
-                                <span>Autore</span>
+                                <span className={`${isCollapsed ? "d-none" : ""}`} onClick={handleToggleLabel}>{selectedLabel}</span>
                                 <button>
-                                    <i className="bi bi-list-ul" onClick={handleToggleLabel}></i>
+                                    <i className={`bi ${selectedIcon}`} onClick={handleToggleLabel}></i>
                                 </button>
 
                                 {/* label */}
-                                {labelShow ? (
+                                {labelShow && (
                                     <div className="order-label">
-                                    <ul className="p-0 m-0">
-                                        <span>Ordina per</span>
-                                        <li>
-                                            <span>Recenti</span>
-                                        </li>
-                                        <li>
-                                            <span>Ordine Alfabetico</span>
-                                        </li>
-                                        <hr className="my-2 border-light" />
-                                        <span>Visualizza come</span>
-                                        <li className="d-flex align-items-center gap-2">
-                                            <i className="bi bi-grid"></i>
-                                            <span>Griglia</span>
-                                        </li>
-                                        <li className="d-flex align-items-center gap-2">
-                                            <i className="bi bi-list-task"></i>
-                                            <span>Elenco</span>
-                                        </li>
-                                        <li className="d-flex align-items-center gap-2">
-                                            <i className="bi bi-list"></i>
-                                            <span>Compatta</span>
-                                        </li>
-                                    </ul>
-                                </div>
-                                ):(
-                                    <div></div>
+                                        <ul className="p-0 m-0">
+                                            <span>Ordina per</span>
+                                            <li onClick={() => handleLabelClick('Recenti', `${selectedIcon}`)}>
+                                                <span>Recenti</span>
+                                            </li>
+                                            <li onClick={() => handleLabelClick('Autore', `${selectedIcon}`)}>
+                                                <span>Autore</span>
+                                            </li>
+                                            <li onClick={() => handleLabelClick('Ordine Alfabetico', `${selectedIcon}`)}>
+                                                <span>Ordine Alfabetico</span>
+                                            </li>
+                                            <hr className="my-2 border-light" />
+                                            <span>Visualizza come</span>
+                                            <li
+                                                className="d-flex align-items-center gap-2"
+                                                onClick={() => handleLabelClick(`${selectedLabel}`, 'bi-grid')}
+                                            >
+                                                <i className="bi bi-grid"></i>
+                                                <span>Griglia</span>
+                                            </li>
+                                            <li
+                                                className="d-flex align-items-center gap-2"
+                                                onClick={() => handleLabelClick(`${selectedLabel}`, 'bi-list-task')}
+                                            >
+                                                <i className="bi bi-list-task"></i>
+                                                <span>Elenco</span>
+                                            </li>
+                                            <li
+                                                className="d-flex align-items-center gap-2"
+                                                onClick={() => handleLabelClick(`${selectedLabel}`, 'bi-list')}
+                                            >
+                                                <i className="bi bi-list"></i>
+                                                <span>Compatta</span>
+                                            </li>
+                                        </ul>
+                                    </div>
                                 )}
                             </div>
                         </div>
-                        <div className="results">
-                            <ul className="m-0 p-0">
-                                {filteredData.map((item) => (
-                                    <li key={item.id} className="d-flex">
-                                        <img src={item.img} alt="" />
-                                        <div className="info d-flex flex-column ms-3">
-                                            <span className="title-item">{item.name}</span>
-                                            <span className="subtitle-item">{item.type}</span>
+
+                        {selectedIcon === 'bi-grid' ? (
+                            <div className="results">
+                                <div className="px-2 d-flex flex-wrap">
+                                    {filteredData.map((item) => (
+                                        <div key={item.id} className={`d-flex flex-column p-2 ${isCollapsed ? "col-6" : "col-3"}`}>
+                                            <img src={item.img} className="w-100" alt="" />
+                                            <div className="info d-flex justify-content-start flex-column mt-2">
+                                                <span className="title-item">{item.name}</span>
+                                                <span className="subtitle-item">{item.type}</span>
+                                            </div>
                                         </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        ) : selectedIcon === 'bi-list-task' ? (
+                            <div className="results">
+                                <ul className="m-0 p-0">
+                                    {filteredData.map((item) => (
+                                        <li key={item.id} className="d-flex">
+                                            <img src={item.img} alt="" />
+                                            <div className="info d-flex flex-column ms-3">
+                                                <span className="title-item">{item.name}</span>
+                                                <span className="subtitle-item">{item.type}</span>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ) : selectedIcon === 'bi-list' ? (
+                            <div className="results">
+                                <ul className="m-0 p-0">
+                                    {filteredData.map((item) => (
+                                        <li key={item.id} className="d-flex">
+                                            <div className="info d-flex align-items-center">
+                                                <span className="title-item me-2">{item.name}</span>
+                                                <i className="bi bi-dot align-middle"></i>
+                                                <span className="subtitle-item align-middle">{item.type}</span>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ) : (
+                            <></>
+                        )}
                     </div>
                 </div>
             </div>
